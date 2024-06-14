fc3739adf3fafb7ecea6c80cf4672a285a612834
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2024-06-14 10:13:50 +0800

    GPS

--- dashboard/admin/controller/gps.php
+++ dashboard/admin/controller/gps.php
@@ -24,55 +24,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo 'GPS ID missing';
     }
 } else {
-    // Check if request is to get data for all devices
-    if (isset($_GET['all_devices']) && $_GET['all_devices'] == 'true') {
-        $allData = [];
-        foreach (glob($dataDir . '*.json') as $filename) {
-            $gpsData = json_decode(file_get_contents($filename), true);
-            $currentTime = time();
-            $dataAge = $currentTime - $gpsData['timestamp'];
+    // Serve the latest GPS data for all devices
+    $allData = [];
+    foreach (glob($dataDir . '*.json') as $filename) {
+        $gpsData = json_decode(file_get_contents($filename), true);
+        $currentTime = time();
+        $dataAge = $currentTime - $gpsData['timestamp'];
 
-            if ($dataAge <= $timeoutDuration) {
-                $allData[] = $gpsData;
-            }
+        if ($dataAge <= $timeoutDuration) {
+            $allData[] = $gpsData;
         }
-        header('Content-Type: application/json');
-        echo json_encode($allData);
-    } else if (isset($_GET['gps_id'])) {
-        // Serve the latest GPS data for the specified device
-        $deviceId = $_GET['gps_id'];
-        $dataFile = $dataDir . $deviceId . '.json';
-
-        if (file_exists($dataFile)) {
-            $gpsData = json_decode(file_get_contents($dataFile), true);
-            $currentTime = time();
-            $dataAge = $currentTime - $gpsData['timestamp'];
-
-            if ($dataAge > $timeoutDuration) {
-                echo json_encode([
-                    'gps_status' => 'No GPS data found',
-                    'latitude' => 0.0,
-                    'longitude' => 0.0,
-                    'speed' => 0.0, // Default speed value
-                    'satellites' => 0, // Default number of satellites
-                    'gps_id' => $deviceId // Include the GPS ID
-                ]);
-            } else {
-                header('Content-Type: application/json');
-                echo json_encode($gpsData);
-            }
-        } else {
-            echo json_encode([
-                'gps_status' => 'No GPS data found',
-                'latitude' => 0.0,
-                'longitude' => 0.0,
-                'speed' => 0.0, // Default speed value
-                'satellites' => 0, // Default number of satellites
-                'gps_id' => $deviceId // Include the GPS ID
-            ]);
-        }
-    } else {
-        echo 'Device ID missing';
     }
+    header('Content-Type: application/json');
+    echo json_encode($allData);
 }
 ?>
